d55244ca01b6ea163827943945589b0a707820d3
Francis-Lam <42219784+Francis-Lam@users.noreply.github.com>
2021-12-13 01:43:58 -0500

    Updates
    quotes generator

--- Final/script.js
+++ Final/script.js
@@ -189,45 +189,42 @@ function pauseAudio() {
   x.pause();  
 }
 
+//Quotes
 
-//Random Activity
-window.onload = () => {
-    randomActivity()
+const text=document.getElementById("quote");
+const author=document.getElementById("author");
+const tweetButton=document.getElementById("tweet");
+
+const getNewQuote = async () =>
+{
+    //api for quotes
+    var url="https://type.fit/api/quotes";    
+
+    // fetch the data from api
+    const response=await fetch(url);
+    console.log(typeof response);
+    //convert response to json and store it in quotes array
+    const allQuotes = await response.json();
+
+    // Generates a random number between 0 and the length of the quotes array
+    const indx = Math.floor(Math.random()*allQuotes.length);
+
+    //Store the quote present at the randomly generated index
+    const quote=allQuotes[indx].text;
+
+    //Store the author of the respective quote
+    const auth=allQuotes[indx].author;
+
+    if(auth==null)
+    {
+        author = "Anonymous";
+    }
+
+    //function to dynamically display the quote and the author
+    text.innerHTML=quote;
+    author.innerHTML="~ "+auth;
+
+    //tweet the quote
+    tweetButton.href="https://twitter.com/intent/tweet?text="+quote+" ~ "+auth;
 }
-
-/*fetch
-const randomActivity = () => {
-    fetch('https://www.boredapi.com/api/activity')
-    .then((res) => {
-        return res.json()
-    }).then((data) => {
-        console.log(data)
-        showRandomActivity(data)
-    })
-}
-
-showRandomActivity = (randomThing) => {
-		document.getElementById('activity').innerText = `${randomThing.results[0].activity}`
-		alert(activity);
-		randomtext = document
-}
-
-/*
-$.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
-  }
-});
-
-
-$.get("https://www.boredapi.com/api/activity", function(data) {
-				console.log(data);
-				alert(data[1].activity);
-
-				$("p").html(data[1].activity);
-				$(".activity").html(data[1].activity);
-			});
-*/
-
+getNewQuote();
